d1f55bb60ae01aeddf0486cd11790fc9c98c5ac3
DanmarGreen0 <107645270+DanmarGreen0@users.noreply.github.com>
2022-09-30 11:53:10 -0700

    Update HelloWorldApplication.java

--- hello-world/src/main/java/learningspring/helloworld/HelloWorldApplication.java
+++ hello-world/src/main/java/learningspring/helloworld/HelloWorldApplication.java
@@ -13,7 +13,7 @@ public class HelloWorldApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
 	}
-
+<dhhfdllfl>
 	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name){
 		return String.format("Hello %s!", name);
